9e1fda14b178d5a2443b683399cbf6b68fd15475
Ibrahim119aa <ibrahimmemon1709@gmail.com>
2025-05-21 18:53:03 +0500

    Expert Page

--- src/app/sitemap/page.js
+++ src/app/sitemap/page.js
@@ -6,18 +6,18 @@ export async function generateMetadata() {
         description: `SiteMap Page, IMTS - SiteMap`, // Dynamic description
     };
 }
-const getSitemap = async () => {
-    let a = await fetch(`${process.env.API_URL}get-Home-Page`,
-        {
-            next: {
-                revalidate: 3600
-            }
-        }
-    );
-    return a.json();
-}
+// const getSitemap = async () => {
+//     let a = await fetch(`${process.env.API_URL}get-Home-Page`,
+//         {
+//             next: {
+//                 revalidate: 3600
+//             }
+//         }
+//     );
+//     return a.json();
+// }
 const SiteMaps = async () => {
-    let a = await getSitemap();
+    // let a = await getSitemap();
     return (
         <div>
             {/* <SiteMap a={a} /> */}
